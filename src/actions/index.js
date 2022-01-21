ac7479acfceb480b1eb611605017a874fd4788ec
Will Phillips <42WPhillips42@gmail.com>
2022-01-20 20:53:38 -0600

    MVP ~50% completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,48 @@
 import axios from 'axios';
 
+export const FETCH_START = "FETCH_START";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const SET_SMURF = "SET_SMURF";
+export const SET_ERROR = "SET_ERROR";
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
+
+export const getSmurfs = (dispatch) => {
+  return function thunk(){
+    dispatch(fetchStart());
+    axios.get(`http://localhost:3333/smurfs`
+      ).then(resp => {
+        console.log(resp);
+        dispatch(fetchSuccess(resp));
+      }).catch(err => {
+        console.log(err);
+        dispatch(fetchFailure(err));
+      })
+  }
+}
+
+export const fetchStart = () => {
+  return({type: FETCH_START});
+}
+
+export const fetchSuccess = (smurfs) => {
+  return({type: FETCH_SUCCESS, payload: smurfs});
+}
+
+export const fetchFailure = (errorMessage) => {
+  return({type: FETCH_FAIL, payload: errorMessage});
+}
+
+export const setSmurf = (smurf) => {
+  return({type: SET_SMURF, payload: smurf});
+}
+
+export const setError = (error) => {
+  return({type: SET_ERROR, payload: error});
+}
+  
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,51 @@
+import { FETCH_START, FETCH_FAIL, FETCH_SUCCESS, SET_SMURF, SET_ERROR } from "../actions/index";
 
 export const initialState = {
+  smurfs: [],
+  isLoading: false,
+  errorMessage: ''
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action)=>{
+  switch(action.type) {
+    case FETCH_START:
+       return {
+         ...state,
+         isLoading: true,
+         errorMessage: ''
+       }
+    case FETCH_FAIL:
+       return {
+         ...state,
+         isLoading: false,
+         errorMessage: action.payload
+       }
+    case FETCH_SUCCESS:
+       return {
+         ...state,
+         smurfs: action.payload,
+         isLoading: false
+       }
+    case SET_SMURF:
+       return {
+         ...state,
+         smurfs:[...state, {
+           id: Date.now(),
+           name: action.payload.name,
+           position: action.payload.position,
+           nickname: action.payload.nickname,
+           description: action.payload.description
+         }]
+       }
+    case SET_ERROR:
+       return {
+         ...state,
+         errorMessage: action.payload
+      }
+    default:
+      return state;
+  }
 }
-
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
 
